c0833245bf5439793af34ad3c913418d622ee908
Michał Wiszenko <wiszenkom@gmail.com>
2023-05-26 23:16:29 +0200

    remove unused imports

--- src/classifiers/naive_bayes/run.py
+++ src/classifiers/naive_bayes/run.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import accuracy_score, f1_score, classification_report
+from sklearn.metrics import classification_report
 
 from utils import process_data
 
